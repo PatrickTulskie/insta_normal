0a857841970d7c924fb1f8b90faaecdcf05da58e
Patrick Tulskie <patricktulskie@gmail.com>
2013-01-31 01:25:40 -0500

    Gallery buttons should work correctly again.

--- chrome/insta_normalizer.js
+++ chrome/insta_normalizer.js
@@ -6,7 +6,10 @@ function loadInstaNormalizer() {
     $("html").live("click", function() {
       $.each($("#profile-media-modal .imgImg.image"), function(index, item) {
         image = $(item).css("background-image").match(/url\((.*)\)/)[1];
-        $(item).parent().html("<img src='" + image + "' style='visibility:visible' />");
+        
+        $('.normalizer_injected_image').remove();
+        $(item).parent().prepend("<img src='" + image + "' style='visibility:visible; display:block;' class='normalizer_injected_image' />");
+        $(item).css('display', 'none');
       });
     });
   }
